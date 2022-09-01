5301cb1b3bee7c6a3ce9c634b46d67ab6f9099a8
Sultan <sultan_galimov@bk.ru>
2022-09-02 02:11:36 +0500

    end progress

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,21 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.cooperation__point');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
